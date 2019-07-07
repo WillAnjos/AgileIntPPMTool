6ae493a1d7286560593d22da26da7c06525e6e50
Will <willanjosaway@gmail.com>
2019-07-07 15:42:04 -0300

    first sunday commit - first stage of custom exceptions

---
+++ PPMToolFullStack/src/main/java/io/xchangers/ppmtool/sevices/ProjectService.java
@@ -0,0 +1,24 @@
+package io.xchangers.ppmtool.sevices;
+
+import io.xchangers.ppmtool.domain.Project;
+import io.xchangers.ppmtool.exceptions.ProjectIdException;
+import io.xchangers.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+
+    }
+}
