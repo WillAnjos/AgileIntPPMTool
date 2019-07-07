03fff3ae9f4eaa61ff41663e803357dd0a54917b
Will <willanjosaway@gmail.com>
2019-07-07 20:58:40 -0300

    seting upp exeption message of id already exist and id does not exist

--- PPMToolFullStack/src/main/java/io/xchangers/ppmtool/sevices/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/xchangers/ppmtool/sevices/ProjectService.java
@@ -23,7 +23,15 @@ public class ProjectService {
     }
 
     public Project findProjectByIdentifier(String projectId){
-        return projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+
+        }
+
+        return project;
 
     }
 }
